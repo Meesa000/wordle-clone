df0081d6cafaa4ca687b005c14d50f72f6cc39ea
Meesa000 <10476858579+Meesa000@users.noreply.github.com>
2024-05-08 19:02:13 +0100

    add boxes depending on length of word

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
+// number of guesses - determines how many rows of boxes
+let numberOfGuesses = 1;
+let word = 'asim';
+let lengthOfWord = word.length;
+
+
 let guessTiles = document.getElementById('guess-tiles');
 guessTiles.style.display = 'flex';
 guessTiles.style.justifyContent = 'center';
 
 
-let guessBox = document.createElement('div');
-guessBox.style.display = 'flex';
-guessBox.style.width = '50px';
-guessBox.style.height = '50px';
-guessBox.style.border = '2px black solid';
+// creates column of 5 - this represents number of guesses
+for (i=0;i<numberOfGuesses;i++){
+    
+    for (j=0;j<lengthOfWord;j++){
+        let guessBoxRow = document.createElement('input');
+        guessBoxRow.style.display = 'flex';
+        guessBoxRow.style.width = '50px';
+        guessBoxRow.style.height = '50px';
+        guessBoxRow.style.border = '2px black solid';
+        guessBoxRow.setAttribute('type','text');
+        guessTiles.appendChild(guessBoxRow);  
+        
+        
+    }
+}
+   
 
 
-guessTiles.appendChild(guessBox);
-console.log(guessTiles);
+
+
+
