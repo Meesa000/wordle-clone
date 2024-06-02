899ad7048c30a7545a9164838650f90b000f5336
Meesa000 <10476858579+Meesa000@users.noreply.github.com>
2024-06-02 14:23:01 +0100

    add guess feature for guess tiles

--- main.js
+++ main.js
@@ -1,29 +1,58 @@
 // number of guesses - determines how many rows of boxes
 let numberOfGuesses = 1;
-let word = 'asim';
+let word = 'five';
 let lengthOfWord = word.length;
 
-
+// container for the letter tiles
 let guessTiles = document.getElementById('guess-tiles');
 guessTiles.style.display = 'flex';
+guessTiles.style.gap = '10px';
 guessTiles.style.justifyContent = 'center';
-
+guessTiles.style.alignItems = 'center';
 
 // creates column of 5 - this represents number of guesses
-for (i=0;i<numberOfGuesses;i++){
+let inputElements = [];
+for (i=0;i<lengthOfWord;i++){
     
-    for (j=0;j<lengthOfWord;j++){
-        let guessBoxRow = document.createElement('input');
-        guessBoxRow.style.display = 'flex';
-        guessBoxRow.style.width = '50px';
-        guessBoxRow.style.height = '50px';
-        guessBoxRow.style.border = '2px black solid';
-        guessBoxRow.setAttribute('type','text');
-        guessTiles.appendChild(guessBoxRow);  
-        
-        
+     let guessBoxRow = document.createElement('input');
+     guessBoxRow.style.display = 'flex';
+     guessBoxRow.style.width = '70px';
+     guessBoxRow.style.height = '70px';
+     guessBoxRow.style.border = '2px black solid';
+     guessBoxRow.style.borderRadius = '4px';
+     guessBoxRow.style.flex.gap = '0px 1em'
+     guessBoxRow.setAttribute('type','text');
+     guessTiles.appendChild(guessBoxRow);  
+
+     inputElements.push(guessBoxRow);
+     
+}
+
+// guess button, when clicked it console logs the words in each letter tile
+let guessBtn = document.createElement('button');
+guessBtn.innerHTML = 'GUESS';
+guessBtn.style.height = '50px';
+guessBtn.style.border = '2px solid black';
+guessBtn.style.borderRadius = '5px';
+guessBtn.onclick = function() {onGuessBtnClick()};
+guessTiles.appendChild(guessBtn);
+
+function onGuessBtnClick(){
+    let guessedWord = '';
+    // Collect the values from each input element
+    inputElements.forEach(input => {
+        guessedWord += input.value;
+    });
+    if (guessedWord == word){
+        console.log('you win');
     }
 }
+
+
+
+
+
+
    
 
 
