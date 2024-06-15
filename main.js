8e4d50e8bc64ac52fe69c9327347199fbefd6b17
Meesa000 <10476858579+Meesa000@users.noreply.github.com>
2024-06-15 20:52:15 +0100

    add more rows

--- main.js
+++ main.js
@@ -1,35 +1,54 @@
 
 // number of guesses - determines how many rows of boxes
 let numberOfGuesses = 1;
-let word = ['l','o','l'];
+let word = ['l','o','l','l','l'];
 let lengthOfWord = word.length;
 
 // container for the letter tiles
-let guessTiles = document.getElementById('guess-tiles');
-guessTiles.style.display = 'flex';
-guessTiles.style.gap = '10px';
-guessTiles.style.justifyContent = 'center';
-guessTiles.style.alignItems = 'center';
+let guessBoxContainer = document.getElementById('guess-tiles');
+guessBoxContainer.style.display = 'flex';
+guessBoxContainer.style.flexDirection = 'column';
+guessBoxContainer.style.gap = '10px';
+guessBoxContainer.style.justifyContent = 'center';
+guessBoxContainer.style.alignItems = 'center';
+
 
 
 // creates column of word length - this represents number of guesses
 let inputElements = [];
-for (i=0;i<lengthOfWord;i++){
+let guessTilesElements = [];
+for (j=0; j<5; j++){
+    let rowContainer = document.createElement('div');
+    rowContainer.style.display = 'flex';
+    rowContainer.style.flexWrap = 'wrap';
+    rowContainer.style.gap = '10px';
+    rowContainer.style.justifyContent = 'center';
+    rowContainer.style.alignItems = 'center';
+    guessTilesElements.push(rowContainer);
+    guessBoxContainer.appendChild(rowContainer);  // append each row to the main container
+    console.log(guessTilesElements);
+// pushed each guesstiles div to an array, each will indicate 
+
+    for (i=0;i<lengthOfWord;i++){
     
-     let guessBoxes = document.createElement('input');
-     guessBoxes.style.fontSize = '40px';
-     guessBoxes.style.textAlign = 'center';
-     guessBoxes.style.display = 'flex';
-     guessBoxes.style.width = '70px';
-     guessBoxes.style.height = '70px';
-     guessBoxes.style.border = '2px black solid';
-     guessBoxes.style.borderRadius = '4px';
-     guessBoxes.style.flex.gap = '0px 1em'
-     guessBoxes.setAttribute('type','text');
-     guessBoxes.style.textTransform = 'capitalize';
-     guessTiles.appendChild(guessBoxes);  
-     inputElements.push(guessBoxes); 
+        let guessBoxes = document.createElement('input');
+        guessBoxes.style.fontSize = '40px';
+        guessBoxes.style.textAlign = 'center';
+        guessBoxes.style.display = 'flex';
+        guessBoxes.style.width = '70px';
+        guessBoxes.style.height = '70px';
+        guessBoxes.style.border = '2px black solid';
+        guessBoxes.style.borderRadius = '4px';
+        guessBoxes.style.flex.gap = '0px 1em'
+        guessBoxes.setAttribute('type','text');
+        guessBoxes.style.textTransform = 'capitalize';
+        rowContainer.appendChild(guessBoxes);  
+        inputElements.push(guessBoxes); 
+   }
+
 }
+
+
 
 // guess button, when clicked it console logs the words in each letter tile
 let guessBtn = document.createElement('button');
@@ -38,7 +57,7 @@ guessBtn.style.height = '50px';
 guessBtn.style.border = '2px solid black';
 guessBtn.style.borderRadius = '5px';
 guessBtn.onclick = function() {onGuessBtnClick()};
-guessTiles.appendChild(guessBtn);
+guessBoxContainer.appendChild(guessBtn);
 
 
 function onGuessBtnClick(){
@@ -51,13 +70,10 @@ function onGuessBtnClick(){
         let correctWord = [];
         for (i=0;i<inputElements.length;i++){
             console.log(element.value);
-            
             if (word.includes(element.value)){
                 element.style.backgroundColor = '#a2c11c'; 
                 correctWord.push(inputElements[i]);
-
-                // if word includes any element value it will put all elements into the correct word array even if wrong since the one of them is correct
-                      
+                // if word includes any element value it will put all elements into the correct word array even if wrong since the one of them is correct      
             }
             else {
                 inputElements[i].style.backgroundColor = 'red'
