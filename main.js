ab1664d7e98cc129f25693bbca9cb5aa715c8145
Meesa000 <10476858579+Meesa000@users.noreply.github.com>
2024-06-15 11:56:30 +0100

    f

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+
 // number of guesses - determines how many rows of boxes
 let numberOfGuesses = 1;
-let word = 'five';
+let word = 'as';
 let lengthOfWord = word.length;
 
 // container for the letter tiles
@@ -10,22 +11,26 @@ guessTiles.style.gap = '10px';
 guessTiles.style.justifyContent = 'center';
 guessTiles.style.alignItems = 'center';
 
+function changeTextToUpper(){
+    console.log('upper')
+    
+}
 // creates column of 5 - this represents number of guesses
 let inputElements = [];
 for (i=0;i<lengthOfWord;i++){
     
-     let guessBoxRow = document.createElement('input');
-     guessBoxRow.style.display = 'flex';
-     guessBoxRow.style.width = '70px';
-     guessBoxRow.style.height = '70px';
-     guessBoxRow.style.border = '2px black solid';
-     guessBoxRow.style.borderRadius = '4px';
-     guessBoxRow.style.flex.gap = '0px 1em'
-     guessBoxRow.setAttribute('type','text');
-     guessTiles.appendChild(guessBoxRow);  
-
-     inputElements.push(guessBoxRow);
-     
+     let guessBoxes = document.createElement('input');
+     guessBoxes.style.fontSize = '40px';
+     guessBoxes.style.textAlign = 'center';
+     guessBoxes.style.display = 'flex';
+     guessBoxes.style.width = '70px';
+     guessBoxes.style.height = '70px';
+     guessBoxes.style.border = '2px black solid';
+     guessBoxes.style.borderRadius = '4px';
+     guessBoxes.style.flex.gap = '0px 1em'
+     guessBoxes.setAttribute('type','text');
+     guessTiles.appendChild(guessBoxes);  
+     inputElements.push(guessBoxes); 
 }
 
 // guess button, when clicked it console logs the words in each letter tile
@@ -37,15 +42,29 @@ guessBtn.style.borderRadius = '5px';
 guessBtn.onclick = function() {onGuessBtnClick()};
 guessTiles.appendChild(guessBtn);
 
+
 function onGuessBtnClick(){
     let guessedWord = '';
     // Collect the values from each input element
-    inputElements.forEach(input => {
-        guessedWord += input.value;
-    });
-    if (guessedWord == word){
-        console.log('you win');
-    }
+    inputElements.forEach(element => {
+        guessedWord += element.value;
+        for (i=0;i<inputElements.length;i++){
+            if (guessedWord == word){
+                inputElements[i].style.backgroundColor = '#a2c11c';   
+                      
+            }
+        }      
+    });    
+}
+
+// dark mode
+let darkModeBtn = document.getElementById('dark-mode');
+darkModeBtn.onclick = function() {darkModeOn()};
+function darkModeOn(){
+    let body = document.body;
+    body.style.backgroundColor = 'grey';
+    body.style.color = 'white';
+    let border = document.querySelectorAll('border');
 }
 
 
